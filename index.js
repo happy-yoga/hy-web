c45d58924fed379e221158566a46cdcf2c9d294e
Falk Hoppe <falkhoppe81@gmail.com>
2022-06-18 09:41:22 +0200

    add rendering of contentful pages

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import { NamedRouter, routes } from 'reversical'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 
-import { router as landingPageRouter } from './lib/controllers/landing-page.js'
+import { init as initializeContentful } from './lib/clients/contentful.js'
+import { router as pagesRouter } from './lib/controllers/pages.js'
 
 import app from './lib/app.js'
 
+await initializeContentful()
+
+app.locals = app.locals || {}
 app.locals.routes = routes
+app.locals.documentToHtmlString = documentToHtmlString
 
 const namedRouter = new NamedRouter(app)
 
-namedRouter.use('landing-page', '/', landingPageRouter)
+namedRouter.use('landing-page', '/', pagesRouter)
 
 export default app
