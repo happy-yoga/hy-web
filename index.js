620f554f579d6a0278a6443632a649133d761864
Falk Hoppe <falkhoppe81@gmail.com>
2022-08-10 14:00:23 +0200

    fix asset lookup for favicon

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import basicAuth from 'express-basic-auth'
 
 import { NamedRouter, routes } from 'reversical'
 import { capture } from './lib/helpers/app-helpers.js'
+import { assetUrl } from './lib/assets.js'
 
 import config from './lib/config.js'
 import { init as initializeContentful, documentToHtmlString } from './lib/clients/contentful.js'
-import { init as initComponentLibAssets, assetUrl } from './lib/assets.js'
+
 import { router as pagesRouter } from './lib/controllers/pages.js'
 import { router as webHookRouter } from './lib/controllers/web-hooks.js'
 import { router as redirectsRouter } from './lib/controllers/redirects.js'
@@ -16,19 +17,8 @@ import { PageList } from './lib/models/page-list.js'
 import { sitemapController } from './lib/controllers/sitemap.js'
 
 await initializeContentful()
-await initComponentLibAssets()
-
-app.locals.routes = routes
 app.locals.documentToHtmlString = documentToHtmlString
-
-if (config.localAssetDevelopment) {
-  app.use(async (_req, _res, next) => {
-    await initComponentLibAssets() // reload assets on each request
-    return next()
-  })
-}
-
-app.locals.assetUrl = assetUrl
+app.locals.routes = routes
 
 app.use((_req, _res, next) => {
   app.locals.mainMenu = PageList.findBySlug('main-menu')
